c178976e6e302a82d2bbaacce982a98521800862
Abdollah Zamannoun <abbezz@live.com>
2022-06-15 06:09:33 +0100

    Error in server.js file

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require("express-session");
+const bodyParser = require('body-parser');
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+
+
+const path = require('path');
+const db = require('./models');
+
+require('dotenv').config();
+
+// Get the table
+(async () => {
+    await db.sequelize.sync();
+})();
+
+app.use(session(sess));
+
+//Handlebars (non Handlebars bug requires InsecurePrototypeAccess to work)
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
