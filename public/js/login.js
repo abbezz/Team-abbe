cc69d8b0ef3a27a76e4829e5d00f5f257b783093
Abdollah Zamannoun <abbezz@live.com>
2022-06-26 18:15:38 +0100

    working on login.js file

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
         headers: { 'Content-Type': 'application/json' },
       });
 
-      
+
       if (response.ok) {
         // If successful, redirect the browser to the profile page
         document.location.replace('/profile');
@@ -23,3 +23,14 @@ const loginFormHandler = async (event) => {
       }
     }
   };
+
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector("#name-signup").value.trim();
+
+    const email = document.querySelector("#email-signup").value.trim();
+
+    const password = document.querySelector("#password-signup").value.trim();
+ 
+
